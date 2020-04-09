6a1a617173ff9cf351350cf087765e7d3ff43b1e
takefumi24 <t_ohba24@yahoo.co.jp>
2020-04-09 17:55:31 +0900

    ゲーム初期段階

--- js/main.js
+++ js/main.js
@@ -1,10 +1,17 @@
 'use strict';
-
 {
   class Panel {
     constructor() {
       this.el = document.createElement('li');
       this.el.classList.add('pressed');
+    }
+    getEl() {
+      return this.el;
+    }
+
+    activate(num) {
+      this.el.classList.remove('pressed');
+      this.el.textContent = num;
     }
   }
 
@@ -12,10 +19,28 @@
     constructor() {
       this.panels = [];
       for (let i = 0; i < 4; i++) {
-        this.panels.push(new this.panels());
+        this.panels.push(new Panel());
       }
+      this.setup();
+    }
+    setup() {
+      const board = document.getElementById('board');
+      this.panels.forEach(panel => {
+        board.appendChild(panel.getEl());
+      });
+    }
+
+    activate() {
+      this.panels.forEach(panel => {
+        panel.activate(0);
+      });
     }
   }
 
   const board = new Board();
+
+  const btn = document.getElementById('btn');
+  btn.addEventListener('click', () => {
+    board.activate();
+  });
 }
